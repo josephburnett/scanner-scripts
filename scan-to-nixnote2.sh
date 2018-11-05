fb87d7f54298b43a014f4da95bc6d0c36ed67f99
Joseph Burnett <josephburnett79@gmail.com>
2018-11-04 18:00:39 -0800

    Add scan-to-nixnote2.sh.

---
+++ scan-to-nixnote2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+TITLE=$@
+if [[ -z "${TITLE?}" ]]; then
+    echo "Usage: scan-to-nixnote2.sh <title>"
+    exit 1
+fi
+ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+DIR="/tmp/scan/$ID"
+echo "Scanning \"${TITLE?}\" to ${ID?} in ${DIR}"
+
+mkdir -p $DIR
+cd $DIR
+function finish {
+    cd /
+    rm $DIR/*
+    rmdir $DIR
+}
+trap finish EXIT
+
+scanimage --device 'fujitsu:ScanSnap iX500:316724' --batch --source 'ADF Duplex'
+
+convert *.pnm out.pdf
+
+nixnote2 \
+    addNote \
+    --notebook='_Unfiled' \
+    --title="$TITLE" \
+    --noteText="$TITLE" \
+    --attachment="$DIR/out.pdf"
+
